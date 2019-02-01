5c96c78f081c26a0c8a0ee9098c95665e59992fd
davidngugi <ndavidngugi@gmail.com>
2019-02-01 08:02:34 +0300

    Add phpunit tests

---
+++ tests/MiniTest.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MiniWrap\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+use MiniWrap\Mini;
+
+final class MiniTest extends TestCase
+{
+
+    public function testDatabaseSettingsAreSet()
+    {
+        return $this->assertTrue(true);
+    }
+
+}
